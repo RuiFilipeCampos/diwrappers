e6c1ccc492fde01ca9b333757ad04668f20b02fa
Rui Campos <mail@ruicampos.org>
2025-01-14 21:43:35 +0100

    feat: add async contextual dependency

--- diwrappers/__init__.py
+++ diwrappers/__init__.py
@@ -1,5 +1,6 @@
 """Entrypoint of diwrappers."""
 
+from ._asybc_contextual_dependency import async_contextual_dependency
 from ._async_dependency import async_dependency
 from ._commons._exceptions import DependencyInjectionError, DependencyLeakError, MissingContextError
 from ._contextual_dependency import contextual_dependency
@@ -9,6 +10,7 @@ __all__ = [
     "DependencyInjectionError",
     "DependencyLeakError",
     "MissingContextError",
+    "async_contextual_dependency",
     "async_dependency",
     "contextual_dependency",
     "dependency",
