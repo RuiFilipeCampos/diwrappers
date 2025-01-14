379d0d8077a76f3837328d173ee414c4ff4228a0
Rui Campos <mail@ruicampos.org>
2025-01-14 07:37:49 +0100

    feat: expose all exceptions

--- diwrappers/__init__.py
+++ diwrappers/__init__.py
@@ -3,5 +3,13 @@
 from ._async_dependency import async_dependency
 from ._contextual_dependency import contextual_dependency
 from ._dependency import dependency
+from ._exceptions import DependencyInjectionError, DependencyLeakError, MissingContextError
 
-__all__ = ["async_dependency", "contextual_dependency", "dependency"]
+__all__ = [
+    "DependencyInjectionError",
+    "DependencyLeakError",
+    "MissingContextError",
+    "async_dependency",
+    "contextual_dependency",
+    "dependency",
+]
